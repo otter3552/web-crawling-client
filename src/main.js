0347d6b20799a310b5261e7631060b0d015835a2
shlee3552 <mr_shield@cpst.co.kr>
2021-02-16 10:10:56 +0900

    lineChart Componet 추가

--- src/main.js
+++ src/main.js
@@ -2,8 +2,10 @@ import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
 import vuetify from './plugins/vuetify';
+import LineChart from './components/charts/LineChart.js'
 
 Vue.config.productionTip = false
+Vue.component('line-chart',LineChart)
 
 new Vue({
   router,
--- src/views/MNG/MNG001.vue
+++ src/views/MNG/MNG001.vue
@@ -3,21 +3,50 @@
     <v-subheader>
       메모리 관리 > memoryMenu 1
     </v-subheader>
-    차트들...
+    
+    <line-chart width="400" height="100" :chart-data="datacollection"></line-chart>
+    <button @click="fillData()">Randomize</button>
+    {{datacollection}}
   </v-container>
 </template>
 
 <script>
 /* eslint-disable */
-export default {  
-  data: () => ({
-    
-  }),
-  mounted () {
-  
-  },
-  methods: {
+
+
+export default {
+  components: {
 
   },
+  data () {
+    return {
+      datacollection: null
+    }
+  },
+  mounted () {
+    this.fillData()
+  },
+  methods: {
+    fillData () {
+      this.datacollection = {
+        labels: ['A','B','C','D'],
+        datasets: [
+          {
+            label: 'Data1',
+            data: [20,50,5,2],
+            borderColor : 'red',
+            backgroundColor : 'rgba(0, 0, 0, 0.1)'
+          },
+          {
+            label: 'Data2',
+            data: [10,25,5,10]
+          }
+        ]
+      }
+    },
+    getRandomInt () {
+      return Math.floor(Math.random() * (50 - 5 + 1)) + 5
+    }
+  }
 }
 </script>
